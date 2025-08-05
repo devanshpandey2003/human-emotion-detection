e7f039ff00a6c40f294565af47b5b25f52565bc4
devanshpandey2003 <devanshpandey8900@gmail.com>
2025-08-06 01:20:38 +0530

    training

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 import tensorflow as tf
-from keras.layers import Resizing, Rescaling
+from tensorflow.keras.layers import (
+    InputLayer,
+    Conv2D,
+    BatchNormalization,
+    MaxPool2D,
+    Dropout,
+    Flatten,
+    Dense,
+    Resizing,
+    Rescaling,
+)
+from tensorflow.keras.regularizers import L2
 import matplotlib.pyplot as plt
+
+from tensorflow.keras.metrics import CategoricalAccuracy, TopKCategoricalAccuracy
+from tensorflow.keras.losses import CategoricalCrossentropy
 
 train_directory = "kaggle/train"
 val_directory = "kaggle/test"
@@ -9,6 +23,17 @@ CLASS_NAMES = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"
 CONFIGURATION = {
     "BATCH_SIZE": 32,
     "IM_SIZE": 256,
+    "N_EPOCHS": 10,
+    "LEARNING_RATE": 0.001,
+    "DROPOUT_RATE": 0.0,
+    "REGULARIZATION_RATE": 0.0,
+    "N_FILTERS": 6,
+    "KERNEL_SIZE": 3,
+    "N_STRIDES": 1,
+    "POOL_SIZE": 2,
+    "N_DENSE_1": 100,
+    "N_DENSE_2": 10,
+    "NUM_CLASSES": len(CLASS_NAMES),
 }
 
 
@@ -66,3 +91,72 @@ resize_rescale_layers = tf.keras.Sequential(
 
 
 # Modeling
+
+lenet_model = tf.keras.Sequential(
+    [
+        InputLayer(shape=(CONFIGURATION["IM_SIZE"], CONFIGURATION["IM_SIZE"], 3)),
+        Rescaling(1.0 / 255, name="rescaling"),
+        Conv2D(
+            filters=CONFIGURATION["N_FILTERS"],
+            kernel_size=CONFIGURATION["KERNEL_SIZE"],
+            strides=CONFIGURATION["N_STRIDES"],
+            activation="relu",
+            kernel_regularizer=L2(CONFIGURATION["REGULARIZATION_RATE"]),
+        ),
+        BatchNormalization(),
+        MaxPool2D(
+            pool_size=CONFIGURATION["POOL_SIZE"], strides=CONFIGURATION["N_STRIDES"]
+        ),
+        Dropout(CONFIGURATION["DROPOUT_RATE"]),
+        Conv2D(
+            filters=CONFIGURATION["N_FILTERS"] * 2 + 4,
+            kernel_size=CONFIGURATION["KERNEL_SIZE"],
+            strides=CONFIGURATION["N_STRIDES"],
+            activation="relu",
+            kernel_regularizer=L2(CONFIGURATION["REGULARIZATION_RATE"]),
+        ),
+        BatchNormalization(),
+        MaxPool2D(
+            pool_size=CONFIGURATION["POOL_SIZE"], strides=CONFIGURATION["N_STRIDES"] * 2
+        ),
+        Flatten(),
+        Dense(
+            CONFIGURATION["N_DENSE_1"],
+            activation="relu",
+            kernel_regularizer=L2(CONFIGURATION["REGULARIZATION_RATE"]),
+        ),
+        BatchNormalization(),
+        Dropout(CONFIGURATION["DROPOUT_RATE"]),
+        Dense(
+            CONFIGURATION["N_DENSE_2"],
+            activation="relu",
+            kernel_regularizer=L2(CONFIGURATION["REGULARIZATION_RATE"]),
+        ),
+        BatchNormalization(),
+        Dense(CONFIGURATION["NUM_CLASSES"], activation="sofmax"),
+    ]
+)
+
+lenet_model.summary()
+
+# Training
+
+
+loss_function = CategoricalCrossentropy(from_logits=False)
+matrics = [
+    CategoricalAccuracy(name="accuracy"),
+    TopKCategoricalAccuracy(k=2, name="top_k_accuracy"),
+]
+
+lenet_model.compile(
+    optimizer=tf.keras.optimizers.Adam(learning_rate=CONFIGURATION["LEARNING_RATE"]),
+    loss=loss_function,
+    metrics=matrics,
+)
+
+history = lenet_model.fit(
+    training_dataset,
+    validation_data=validation_dataset,
+    epochs=CONFIGURATION["N_EPOCHS"],
+    verbose=1,
+)
